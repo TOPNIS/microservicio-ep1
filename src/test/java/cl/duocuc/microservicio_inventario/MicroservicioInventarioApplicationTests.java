becfae86dcf224ba03c376cbb989190e0190c65f
Junior <ju.altidor@duocuc.cl>
2025-11-04 17:55:16 -0300

    fix: configurar H2 para tests y evitar error de conexion a Oracle

--- src/test/java/cl/duocuc/microservicio_inventario/MicroservicioInventarioApplicationTests.java
+++ src/test/java/cl/duocuc/microservicio_inventario/MicroservicioInventarioApplicationTests.java
@@ -2,8 +2,10 @@ package cl.duocuc.microservicio_inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MicroservicioInventarioApplicationTests {
 
 	@Test
